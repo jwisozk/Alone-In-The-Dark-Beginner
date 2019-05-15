047813236ec2103085eabd4f09388d7f4fec3190
Jacob Kowalski <mr.jacob.kowalski@gmail.com>
2019-05-15 18:46:41 +0300

    Complete options program

---
+++ 5-options/options.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   options.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwisozk <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/15 16:01:19 by jwisozk           #+#    #+#             */
+/*   Updated: 2019/05/15 18:37:20 by jwisozk          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#define LEN 32
+
+size_t	ft_strlen(char const *s)
+{
+    size_t	i;
+
+    i = 0;
+    while (s[i] != '\0')
+        i++;
+    return (i);
+}
+
+void    ft_fill_str(char *str)
+{
+    int i;
+
+    i = 0;
+    while (i < LEN)
+    {
+        str[i] = '0';
+        i++;
+    }
+    str[i] = '\0';
+}
+
+int     ft_valid_opt(int count, char **argv, char *arr)
+{
+    int i;
+    int j;
+
+    ft_fill_str(arr);
+    i = 0;
+    while (i < count)
+    {
+        j = 0;
+        while (argv[i][j] != '\0')
+        {
+            if ((j == 0 && argv[i][j] != '-') ||
+                (j > 0 && argv[i][j] < 97) ||
+                (j > 0 && argv[i][j] > 122))
+                return (2);
+            if (argv[i][j] == 'h')
+                return (0);
+            if (argv[i][j] != '-')
+                arr[argv[i][j] - 97] = '1';
+            j++;
+        }
+        i++;
+    }
+    return (1);
+}
+
+void    ft_usage(char *s)
+{
+    write(1, &*s, ft_strlen(s));
+}
+
+int main(int argc, char **argv)
+{
+    char arr[LEN + 1];
+    int status;
+    int i;
+
+    status = ft_valid_opt(argc - 1, argv + 1, arr);
+    if (argc > 1 && status == 1)
+    {
+        i = LEN;
+        while (i--)
+        {
+            write(1, &arr[i], 1);
+            if (i != 0 && i % 8 == 0)
+                write(1, " ", 1);
+        }
+        write(1, "\n", 1);
+    }
+    else if (status == 2)
+    {
+        ft_usage("Invalid Option\n");
+    }
+    else
+        ft_usage("options: abcdefghijklmnopqrstuvwxyz\n");
+    return 0;
+}
